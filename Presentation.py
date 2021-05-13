ccf6b3c34431288aa44a05f9d42207a55e8a205e
Xanthus84 <Vladimirthebest@yandex.ru>
2021-05-13 16:42:45 +0300

    Изменен принцип создания графиков в PowerPoint только средствами библиотеки pptx

--- Presentation.py
+++ Presentation.py
@@ -1,27 +1,96 @@
-import Excel_report
-
-import win32com.client
-from PIL import ImageGrab
+# Create a presentation with one slide
 from pptx import Presentation
+from pptx.chart.data import ChartData
+from pptx.enum.chart import XL_CHART_TYPE, XL_LEGEND_POSITION, XL_LABEL_POSITION
 from pptx.util import Inches, Pt
-
+import Excel_report
 
 # whatis = lambda obj: print(type(obj), "\n\t" + "\n\t".join(dir(obj)))
 
-def last_cell_value(sheet_2, ranges):
-    value = 0
-    last = 0
-    for v in sheet_2.Range(ranges).Value:
-        if v[0] is None:
-            break
-        value += v[0]
-        last = v[0]
-    if ranges == 'B2:B54' or ranges == 'C2:C54':
-        value = value - last
-    return str(round(value, 1))
+# create presentation with 1 slide ------
+
+def prepare_the_data(ws, column):
+    list_data = []
+    for row in range(2, ws.max_row):  # цикл по строкам с данными
+        list_data.append(ws.cell(row, column).value)
+    return list_data
 
 
-def create_table(sheet_2, slide_table):
+def create_chart_line(ws, slide_num, column_1, column_2, column_3):
+    chart_data = ChartData()
+    chart_data.categories = prepare_the_data(ws, 1)
+    if column_3 == 0 and column_2 != 0:
+        chart_data.add_series(ws.cell(1, column_1).value, prepare_the_data(ws, column_1))
+        chart_data.add_series(ws.cell(1, column_2).value, prepare_the_data(ws, column_2))
+    elif column_3 == 0 and column_2 == 0:
+        chart_data.add_series(ws.cell(1, column_1).value, prepare_the_data(ws, column_1))
+    else:
+        chart_data.add_series(ws.cell(1, column_1).value, prepare_the_data(ws, column_1))
+        chart_data.add_series(ws.cell(1, column_2).value, prepare_the_data(ws, column_2))
+        chart_data.add_series(ws.cell(1, column_3).value, prepare_the_data(ws, column_3))
+
+    x, y, cx, cy = Inches(0.1), Inches(1), Inches(9.85), Inches(4.5)
+    chart = slide_num.shapes.add_chart(
+        XL_CHART_TYPE.LINE, x, y, cx, cy, chart_data
+    ).chart
+
+    chart.has_legend = True
+    chart.legend.include_in_layout = False
+    chart.series[0].smooth = True
+
+
+def create_chart_pie(ws, slide_num):
+    chart_data = ChartData()
+    chart_data.categories = [ws.cell(1, 2).value, ws.cell(1, 3).value]
+    chart_data.add_series("False", (ws.cell(ws.max_row, 2).value, ws.cell(ws.max_row, 3).value))
+
+    x, y, cx, cy = Inches(1), Inches(1), Inches(8), Inches(4.5)
+    chart = slide_num.shapes.add_chart(
+        XL_CHART_TYPE.PIE, x, y, cx, cy, chart_data
+    ).chart
+
+    chart.has_legend = True
+    chart.legend.position = XL_LEGEND_POSITION.RIGHT
+    chart.legend.include_in_layout = False
+
+    chart.plots[0].has_data_labels = True
+    data_labels = chart.plots[0].data_labels
+    data_labels.number_format = '0.0'
+    data_labels.position = XL_LABEL_POSITION.OUTSIDE_END
+
+
+def create_chart_column(ws, slide_num):
+    chart_data = ChartData()
+    chart_data.categories = prepare_the_data(ws, 1)
+    chart_data.add_series(ws.cell(1, 2).value, prepare_the_data(ws, 2))
+    chart_data.add_series(ws.cell(1, 3).value, prepare_the_data(ws, 3))
+    chart_data.add_series(ws.cell(1, 4).value, prepare_the_data(ws, 4))
+    chart_data.add_series(ws.cell(1, 5).value, prepare_the_data(ws, 5))
+    chart_data.add_series(ws.cell(1, 6).value, prepare_the_data(ws, 6))
+    chart_data.add_series(ws.cell(1, 7).value, prepare_the_data(ws, 7))
+    chart_data.add_series(ws.cell(1, 8).value, prepare_the_data(ws, 8))
+
+    x, y, cx, cy = Inches(0.1), Inches(1), Inches(9.85), Inches(4.5)
+    graphic_frame = slide_num.shapes.add_chart(
+        XL_CHART_TYPE.COLUMN_CLUSTERED, x, y, cx, cy, chart_data
+    )
+    chart = graphic_frame.chart
+
+    chart.has_legend = True
+    chart.legend.position = XL_LEGEND_POSITION.RIGHT
+    chart.legend.include_in_layout = False
+    chart.legend.font.size = Pt(12)
+
+    chart.plots[0].has_data_labels = False
+
+
+def sum_column_value(ws, column_num):
+    num = 0
+    for row in range(2, ws.max_row):  # цикл по строкам с данными
+        num += ws.cell(row, column_num).value
+    return str(round(num, 1))
+
+def create_table(ws, slide_table):
     # задание параметров будущей таблицы
     rows = 11
     cols = 3
@@ -53,16 +122,16 @@ def create_table(sheet_2, slide_table):
     table.cell(10, 0).text = 'Расход газа на БКЭУш, всего – н.куб.м'
     table.cell(7, 2).text = 'Последнее ТО – 26.01.21 – 230 мч'
 
-    table.cell(1, 1).text = last_cell_value(sheet_2, 'D2:D54')
-    table.cell(2, 1).text = last_cell_value(sheet_2, 'C2:C54')
-    table.cell(3, 1).text = last_cell_value(sheet_2, 'B2:B54')
-    table.cell(4, 1).text = last_cell_value(sheet_2, 'F2:F54')
-    table.cell(5, 1).text = last_cell_value(sheet_2, 'E2:E54')
-    table.cell(6, 1).text = last_cell_value(sheet_2, 'I2:I54')
-    table.cell(7, 1).text = last_cell_value(sheet_2, 'G2:G54')
-    table.cell(8, 1).text = last_cell_value(sheet_2, 'K2:K54')
-    table.cell(9, 1).text = last_cell_value(sheet_2, 'J2:J54')
-    table.cell(10, 1).text = last_cell_value(sheet_2, 'H2:H54')
+    table.cell(1, 1).text = sum_column_value(ws, 4)
+    table.cell(2, 1).text = sum_column_value(ws, 3)
+    table.cell(3, 1).text = sum_column_value(ws, 2)
+    table.cell(4, 1).text = sum_column_value(ws, 6)
+    table.cell(5, 1).text = sum_column_value(ws, 5)
+    table.cell(6, 1).text = sum_column_value(ws, 9)
+    table.cell(7, 1).text = sum_column_value(ws, 7)
+    table.cell(8, 1).text = sum_column_value(ws, 11)
+    table.cell(9, 1).text = sum_column_value(ws, 10)
+    table.cell(10, 1).text = sum_column_value(ws, 8)
 
     # Формат шрифта ячеек
     for cell in table.iter_cells():
@@ -96,49 +165,16 @@ def create_table(sheet_2, slide_table):
     table.cell(10, 1).margin_left = Inches(0.7)
 
 
-def make_presentations():  # функция сохранения всех графиков в формате png и создания презентации
-    # ------ СОХРАНЕНИЕ ГРАФИКОВ ИЗ ТАБЛИЦЫ EXCEL В ФОРМАТЕ PNG-----------------
-    input_file = "//files.nipom.org/res/Razrab-09/Обмен/АИП/6707-Кузнецк/Тренды/Новокузнецк-2020/Графики.xlsx"
-    output_image = "C:/Razrab-10/python/ExcelWordIntegration/"
+def make_presentations(ws):
 
-    operation = win32com.client.Dispatch("Excel.Application")
-    operation.Visible = 0
-    operation.DisplayAlerts = 0
-
-    workbook_2 = operation.Workbooks.Open(input_file)  # открытие таблицы Графики.xlsx
-    sheet_2 = operation.Sheets(1)  # присвоение переменной sheet_2 параметров первого листа Графики.xlsx
-
-    # создание рисунков из графиков и их сохранение по указанному пути в формате png
-    for x, chart in enumerate(sheet_2.Shapes):
-        chart.Copy()
-        image = ImageGrab.grabclipboard()
-        image.save(output_image + "{}.png".format(x), 'png')
-        pass
-
-    # установление даты начала и конца ОПЭ из таблицы Графики.xlsx
-    date_start = sheet_2.Cells(2, 15).Value.strftime('%d.%m.%Y')
-    date_end = 0
-    for v in sheet_2.Range('P2:P54').Value:
-        if v[0] is None:
-            break
-        date_end = v[0].strftime('%d.%m.%Y')
-
-    # присвоение переменным наименований рисунков и их пути расположения
-    img_path_0 = 'C:/Razrab-10/python/ExcelWordIntegration/0.png'
-    img_path_1 = 'C:/Razrab-10/python/ExcelWordIntegration/1.png'
-    img_path_2 = 'C:/Razrab-10/python/ExcelWordIntegration/2.png'
-    img_path_3 = 'C:/Razrab-10/python/ExcelWordIntegration/3.png'
-    img_path_4 = 'C:/Razrab-10/python/ExcelWordIntegration/4.png'
-    img_path_5 = 'C:/Razrab-10/python/ExcelWordIntegration/5.png'
-    img_path_6 = 'C:/Razrab-10/python/ExcelWordIntegration/6.png'
-    img_path_7 = 'C:/Razrab-10/python/ExcelWordIntegration/7.png'
-
-    img_path_all = [img_path_0, img_path_1, img_path_2, img_path_3, img_path_4, img_path_5, img_path_6,
-                    img_path_7]  # создание списка картинок для слайдов с картинками
+    prs = Presentation("temp_prs.pptx")
+    blank_slide_chart = prs.slide_layouts[0]
+    blank_slide_table = prs.slide_layouts[0]
+    blank_slide_name = prs.slide_layouts[1]
 
     title_name = "Итоги ОПЭ БКЭУ-СМ/ГПЭГ/ГВК в ООО «Газпром добыча Кузнецк»\nна площадке РН-21-22\nза период с {} по {} г.".format(
-        date_start,
-        date_end)  # создание наименования заголовка заглавного слайда с автоматической вставкой дат начала и конца текущего ОПЭ
+        ws.cell(2, 15).value.strftime('%d.%m.%Y'),
+        ws.cell(ws.max_row - 1, 16).value.strftime('%d.%m.%Y'))  # создание наименования заголовка заглавного слайда с автоматической вставкой дат начала и конца текущего ОПЭ
     title_table = "СВОДНЫЙ ОТЧЕТ"  # создание наименования заголовка слайда с таблицей
     # создание наименований заголовков слайдов с картинками
     title_0 = "ДИАГРАММА ИЗМЕРЯЕМЫХ ПРАМЕТРОВ ПО НЕДЕЛЯМ"
@@ -153,54 +189,38 @@ def make_presentations():  # функция сохранения всех гра
     title_all = [title_0, title_1, title_2, title_3, title_4, title_5, title_6,
                  title_7]  # создание списка заголовков слайдов с картинками
 
-    prs = Presentation("temp_prs.pptx")  # открытие шаблона презентации
-    blank_slide_layout = prs.slide_layouts[0]  # задание шаблона слайда из конструктора
-    blank_slide_name = prs.slide_layouts[1]  # задание шаблона слайда из конструктора
-    blank_slide_table = prs.slide_layouts[0]  # задание шаблона слайда из конструктора
-
-    slide_name = prs.slides.add_slide(blank_slide_name)  # создание заглавного слайда
-    slide_table = prs.slides.add_slide(blank_slide_table)  # создание слайда с таблицей
-    slide_0 = prs.slides.add_slide(blank_slide_layout)  # создание слайда с картинкой
-    slide_1 = prs.slides.add_slide(blank_slide_layout)  # создание слайда с картинкой
-    slide_2 = prs.slides.add_slide(blank_slide_layout)  # создание слайда с картинкой
-    slide_3 = prs.slides.add_slide(blank_slide_layout)  # создание слайда с картинкой
-    slide_4 = prs.slides.add_slide(blank_slide_layout)  # создание слайда с картинкой
-    slide_5 = prs.slides.add_slide(blank_slide_layout)  # создание слайда с картинкой
-    slide_6 = prs.slides.add_slide(blank_slide_layout)  # создание слайда с картинкой
-    slide_7 = prs.slides.add_slide(blank_slide_layout)  # создание слайда с картинкой
+    slide_name = prs.slides.add_slide(blank_slide_name)
+    slide_table = prs.slides.add_slide(blank_slide_table)
+    slide_0 = prs.slides.add_slide(blank_slide_chart)  # создание слайда с картинкой
+    slide_1 = prs.slides.add_slide(blank_slide_chart)  # создание слайда с картинкой
+    slide_2 = prs.slides.add_slide(blank_slide_chart)  # создание слайда с картинкой
+    slide_3 = prs.slides.add_slide(blank_slide_chart)  # создание слайда с картинкой
+    slide_4 = prs.slides.add_slide(blank_slide_chart)  # создание слайда с картинкой
+    slide_5 = prs.slides.add_slide(blank_slide_chart)  # создание слайда с картинкой
+    slide_6 = prs.slides.add_slide(blank_slide_chart)  # создание слайда с картинкой
+    slide_7 = prs.slides.add_slide(blank_slide_chart)  # создание слайда с картинкой
 
     slide_all = [slide_0, slide_1, slide_2, slide_3, slide_4, slide_5, slide_6,
                  slide_7]  # создание списка слайдов с картинками
-    # создание заголовка заглавному слайду
-    title = slide_name.shapes.title
-    title.text = title_name
-    # создание заголовка слайду с таблицей
-    title = slide_table.shapes.title
-    title.text = title_table
 
-    for i in range(0, 8):  # создание заголовков слайдов презентаций с картинками
+    title = slide_name.shapes.title  # создание заголовка заглавного слайда презентации
+    title.text = title_name
+    title = slide_table.shapes.title  # создание заголовка слайда презентации с таблицей
+    title.text = title_table
+    for i in range(0, len(slide_all)):  # создание заголовков слайдов презентаций с картинками
         title = slide_all[i].shapes.title
         title.text = title_all[i]
 
-    create_table(sheet_2, slide_table)  # создание слайда с таблицей
 
-    # задание параметров для построения слайда 0 с картинкой
-    top = Inches(1.5)
-    left = Inches(0.1)
-    height = Inches(3.3)
+    create_table(ws, slide_table)
+    create_chart_column(ws, slide_0)
+    create_chart_line(ws, slide_1, 2, 3, 4)  # создание графика слайда "ДИАГРАММА ИЗМЕРЯЕМЫХ ПРАМЕТРОВ ПО НЕДЕЛЯМ"
+    create_chart_line(ws, slide_2, 4, 5, 6)  # создание графика слайда "ВЫРАБОТКА ЭЛЕКТРОЭНЕРГИИ"
+    create_chart_line(ws, slide_3, 8, 10, 11)  # создание графика слайда "ПОТРЕБЛЕНИЕ ГАЗА"
+    create_chart_line(ws, slide_4, 12, 0, 0)  # создание графика слайда "ЭФФЕКТИВНОСТЬ ВЫРАБОТКИ ЭЛЕКТРОЭНЕРГИИ БКЭУ"
+    create_chart_line(ws, slide_5, 9, 0, 0)  # создание графика слайда "ВЫРАБОТКА ТЕПЛОВОЙ ЭНЕРГИИ"
+    create_chart_pie(ws, slide_6)  # создание диаграммы слайда "ВЫРАБОТКА ЗА ОТЧЕТНЫЙ ПЕРИОД"
+    create_chart_line(ws, slide_7, 13, 14, 0)  # создание графика слайда "ТЕМПЕРАТУРА ВНУТРИ БКЭУ"
 
-    for i in range(0, 8):  # создание слайдов презентаций с картинками
-        if i > 0:  # изменение размеров картинок, начиная с 1-й
-            top = Inches(1)
-            left = Inches(0.5)
-            height = Inches(4.5)
-        pic = slide_all[i].shapes.add_picture(img_path_all[i], left, top, height=height)
-
-    # whatis(sheet_2)
     prs.save("C:" + Excel_report.os.path.join(Excel_report.os.environ['HOMEPATH'],
                                               'Desktop') + '\\Промежуточные итоги ОПЭ БКЭУ.pptx')  # сохранение презентации на рабочем столе
-
-    workbook_2.Close(True)  # закрытие рабочей книги
-    operation.Quit()  # окончание работы с COM командами
-    # ----------------------------------------------------------------------------
-    # whatis(sheet_1.values)
